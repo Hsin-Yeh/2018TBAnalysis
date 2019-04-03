3d2947f410636f02c4a85adf1105e3b7e251f2fc
HsinYeh <thankyouyou06@gmail.com>
2019-04-03 16:19:32 +0800

    modified:   src/main.cc

--- src/main.cc
+++ src/main.cc
@@ -87,9 +87,9 @@ void main_makeNtuple(bool Is_Data){
 
 
     ntupleMaker *N;
-    N->Is_Data = Is_Data ;
     if(Is_Data){   N = new ntupleMaker(chain1, chain2, chain3, filename); }
     else       {   N = new ntupleMaker(chain1, chain2, filename);         }
+    N->Is_Data = Is_Data ;
     N->NtupleMaker();
 }
 
