b63627cd23bb8de1801d762b9975733fd4c6ada0
HsinYeh <thankyouyou06@gmail.com>
2019-05-22 21:54:45 +0800

    tmp

--- src/makePlots.cc
+++ src/makePlots.cc
@@ -339,7 +339,7 @@ void makePlots::Loop(){
 	  if ( maxID[ iL ] > 75 && maxID[ iL ] < 100 )
 		h_E1devE7_differentMaxID_2[ iL ] -> Fill( E1devE7 );
 
-	  if ( m_x < -0.00025 && m_x > -0.00045 && m_y > 0.0004 && m_y < 0.0006 ) {
+	  if ( m_x < -0.00032 && m_x > -0.0004 && m_y > 0.0004 && m_y < 0.00056 ) {
 		h_E1devE7_smallAngle [iL] -> Fill ( E1devE7 );
 		h_E7devE19_smallAngle[iL] -> Fill ( E7devE19 );
 	  }
