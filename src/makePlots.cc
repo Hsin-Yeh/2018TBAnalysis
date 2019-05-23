d6715fdfe31e4a2ca4cc5e7d490d4d727bed1897
HsinYeh <thankyouyou06@gmail.com>
2019-05-23 14:35:27 +0800

    tmp

--- src/makePlots.cc
+++ src/makePlots.cc
@@ -229,9 +229,9 @@ void makePlots::Loop(){
   TH1D *h_E1devE7_SecondRing_no_XTalk = new TH1D("h_E1deve7_SecondRing_no_XTalk","",101,0,1.01);
   TH1D *h_SHD_Elayer = new TH1D("h_SHD_Elayer","",50,0,25);
 
-  //TH2D *h_impactX_posx = new TH2D("h_impactX_posx","",50,-6,6,50,-6,6);
-  //TH2D *h_impactY_posy = new TH2D("h_impactY_posy","",50,-6,6,50,-6,6);
-  //TH2D *h_SHD_impactR = new TH2D("h_SHD_impactR","",50,0,25,50,0,5);
+  TH2D *h_impactX_posx = new TH2D("h_impactX_posx","",50,-6,6,50,-6,6);
+  TH2D *h_impactY_posy = new TH2D("h_impactY_posy","",50,-6,6,50,-6,6);
+  TH2D *h_SHD_impactR = new TH2D("h_SHD_impactR","",50,0,25,50,0,5);
   TH2D *h_bx_by = new TH2D("h_bx_by","",500,-60,60,500,-60,60);
   TH2D *h_mx_my = new TH2D("h_mx_my","",100,-0.004,0.004,100,-0.004,0.004);
   TH2D *h_mx_my_E1devE7[EE_NLAYER];
@@ -267,9 +267,9 @@ void makePlots::Loop(){
 	sprintf(title,"layer%i_E7devE19_E1",iL+1);
 	h_E7devE19_E1[iL] = new TH2D(title, title, 101, 0, 1.01, 100, 0, 300);
 	sprintf(title,"layer%i_E1devE7_lessBins",iL+1);
-	h_E1devE7[iL] = new TH1D(title, title, 81, 0, 1.0125);
+	h_E1devE7_lessBins[iL] = new TH1D(title, title, 81, 0, 1.02);
 	sprintf(title,"layer%i_E7devE19_lessBins",iL+1);
-	h_E7devE19[iL] = new TH1D(title, title, 81, 0, 1.0125);
+	h_E7devE19_lessBins[iL] = new TH1D(title, title, 81, 0, 1.02);
 	sprintf(title,"layer%i_E1devE7_smallAngle_lessBins",iL+1);
 	h_E1devE7_smallAngle_lessBins[iL] = new TH1D(title, title, 81, 0, 1.0125);
 	sprintf(title,"layer%i_E7devE19_smallAngle_lessBins",iL+1);
@@ -378,8 +378,8 @@ void makePlots::Loop(){
 	  latShower_bx_by [layer-1] -> Fill ( b_x, b_y, 1 );
 
 	  if ( layer != 1 ) continue;
-	  //h_impactX_posx -> Fill( impactX[0], posx );
-	  //h_impactY_posy -> Fill( impactY[0], posy );
+	  h_impactX_posx -> Fill( impactX[0], posx );
+	  h_impactY_posy -> Fill( impactY[0], posy );
 	  
 	  //Energy_cell [ chip*32 + channel/2 ] += energy;
 	  //latShower_hits [ layer - 1 ] -> Fill( posx, posy, energy );
