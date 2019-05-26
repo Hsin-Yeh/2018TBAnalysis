698705134d52f1e400131af4c045a9c0d7213d79
HsinYeh <thankyouyou06@gmail.com>
2019-05-26 22:37:03 +0800

    tmp

--- src/makePlots.cc
+++ src/makePlots.cc
@@ -346,7 +346,7 @@ void makePlots::Loop(){
 	h_mx_my    -> Fill ( m_x, m_y );
 	h_bx_mx    -> Fill ( b_x, m_x );
 	h_by_my    -> Fill ( b_y, m_y );
-	cout << mx << " " << my << endl;
+	cout << m_x << " " << m_y << endl;
 			
 	for(int iL = 0; iL < EE_NLAYER ; ++iL){ 	  //Fill shower shape histogram
 
