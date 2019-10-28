5cb2276705b93e7fbb67417d50f924b9466ed846
HsinYeh <thankyouyou06@gmail.com>
2019-10-28 14:20:18 +0800

    modified:   src/ntupleMaker.cc

--- src/ntupleMaker.cc
+++ src/ntupleMaker.cc
@@ -552,7 +552,7 @@ void ntupleMaker::NtupleMaker(){
 	    }
 	    double E1devE7_showerAxis =  E_1_showerAxis[iL] / E_7_showerAxis[iL];
 	    double E1devE7 =  E_1[iL] / E_7[iL];
-	    //cout << iL << " " << E1devE7 << " " << E1devE7_showerAxis << " " << endl;
+	    cout << iL << " " << E1devE7 << " " << E1devE7_showerAxis << " " << endl;
 	}
 
 	
