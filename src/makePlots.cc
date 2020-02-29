4673afa54c7a5eccd8ea69d6c77b37d3241d1ce0
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2020-03-01 00:21:35 +0800

    add 200GeV DWC cut

--- src/makePlots.cc
+++ src/makePlots.cc
@@ -463,10 +463,19 @@ void makePlots::Loop(){
 	// Event Selection
 	if ( dwcReferenceType != 13) continue; 
 	if ( totalNhit_CEH > 80 ) continue;
-	if ( abs(b_x) < 1 || abs(b_x-31) < 1 ) {}
-	else continue;
-	if ( abs(b_y) < 1 || abs(b_y-21) < 1 ) {}
-	else continue;
+
+    if ( trueBeamEnergy == 20 ) {
+        if ( abs(b_x) < 1 || abs(b_x-31) < 1 ) {}
+        else continue;
+        if ( abs(b_y) < 1 || abs(b_y-21) < 1 ) {}
+        else continue;
+    }
+    if ( trueBeamEnergy == 200 ) {
+        if ( abs(b_x+1) < 1 || abs(b_x-33.5) < 1 ) {}
+        else continue;
+        if ( abs(b_y) < 1 || abs(b_y-21) < 1 ) {}
+        else continue;
+    }
 	
 	double impact_R = sqrt ( (impactX[0] * impactX[0]) + (impactY[0] * impactY[0]) );
 	//if ( impact_R < 1 ) continue;
