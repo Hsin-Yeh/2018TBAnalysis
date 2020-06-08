33947c13b90f305ce1e2832ac44bee38aaae8700
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2020-06-08 15:41:12 +0800

    add event selection

--- src/makePlots.cc
+++ src/makePlots.cc
@@ -437,7 +437,9 @@ void makePlots::Loop(){
 
         // Event Selection
         if ( dwcReferenceType != 13) continue;
-        if ( totalNhit_CEH > 80 ) continue;
+        int totalNhit_CEH_wo3637 = totalNhit_CEH - layerNhit[35] - layerNhit[36] ;
+        if ( totalNhit_CEH_wo3637 > 25 ) continue;
+        if ( totalE_CEE / totalE < 0.95) continue;
 
         if ( abs(b_x - b_x_mean) < 1 ) {}
         else continue;
