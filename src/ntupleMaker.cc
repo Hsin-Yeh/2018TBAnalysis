398fb7e2a9af0cf2f3b8aff10be83830d409b9e8
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2020-08-04 22:48:36 +0800

    tmp

--- src/ntupleMaker.cc
+++ src/ntupleMaker.cc
@@ -562,13 +562,12 @@ void ntupleMaker::NtupleMaker(){
                     // E_1[iL] = E_ch[iL][ich];
                 // }
             // }
-            cout << (int)hit_tmp[iL-1].size() << endl;
-            for (int idx = 0; idx < (int)hit_tmp[iL-1].size(); idx++){
-                if ( hit_tmp[iL-1].at(idx) > Emax ) {
-                    Emax = hit_tmp[iL-1].at(idx);
-                    cout << hit_tmp[iL-1].at(idx) << endl;
+            for (int idx = 0; idx < (int)hit_tmp[iL].size(); idx++){
+                if ( hit_tmp[iL].at(idx) > Emax ) {
+                    Emax = hit_tmp[iL].at(idx);
+                    cout << hit_tmp[iL].at(idx) << endl;
                     maxID[iL] = idx;
-                    E_1[iL] = hit_tmp[iL-1].at(idx);
+                    E_1[iL] = hit_tmp[iL].at(idx);
                 }
             }
             cout << "hi" << endl;
