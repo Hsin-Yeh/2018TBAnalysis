19d6b60c442f349a8ac1aba4d557c6c85acae97a
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2020-09-07 16:31:55 +0800

    change title font

--- xtalk_compare.C
+++ xtalk_compare.C
@@ -53,7 +53,7 @@ void xtalk_compare(int Energy)
         E1devE7__1->SetMarkerColor(2);
         E1devE7__1->SetMarkerStyle(20);
         E1devE7__1->SetMarkerSize(0.5);
-        E1devE7__1->GetXaxis()->SetTitle("E(r0)/E(r1)");
+        E1devE7__1->GetXaxis()->SetTitle("#font[12]{Times Italic}{E(r0)/E(r1)}");
         E1devE7__1->GetXaxis()->SetLabelFont(42);
         E1devE7__1->GetXaxis()->SetLabelOffset(0.01125);
         E1devE7__1->GetXaxis()->SetLabelSize(0.045);
