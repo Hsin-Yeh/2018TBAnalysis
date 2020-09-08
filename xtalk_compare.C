0317c8338ca9520d93588577b51e5b01aa7a8e91
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2020-09-08 18:49:05 +0800

    chagne config

--- xtalk_compare.C
+++ xtalk_compare.C
@@ -6,13 +6,13 @@ void xtalk_compare(int Energy)
     char title[200];
     //int Energy = 300;
     
-    sprintf(title,"root_plot/plot_sim_%dGeV_EMN_fullbeamline_v46_5.root",Energy);
+    sprintf(title,"root_plot/DWCfixedcut_bx_27_36/plot_sim_%dGeV_EMN_fullbeamline_v46_5.root",Energy);
     // sprintf(title,"root_plot/plot_ntuple_sim_config22_pdgID11_beamMomentum%d_listFTFP_BERT_EMN_LeadNearHGCAL.root",Energy);
     TFile f_MC(title);
     sprintf(title,"root_plot/plot_data_%dGeV_Ele.root",Energy);
     TFile f_Data(title);
     // sprintf(title,"root_plot/plot_ntuple_sim_config22_pdgID11_beamMomentum%d_listFTFP_BERT_EMN.root",Energy);
-    sprintf(title,"root_plot/plot_ntuple_sim_config22_pdgID11_beamMomentum%d_listFTFP_BERT_EMN_0000_4.root",Energy);
+    sprintf(title,"root_plot/DWCfixedcut_bx_27_36/plot_ntuple_sim_config22_pdgID11_beamMomentum%d_listFTFP_BERT_EMN_0000_4.root",Energy);
     TFile f_MC_original(title);
 
     int NLAYER = 28;
